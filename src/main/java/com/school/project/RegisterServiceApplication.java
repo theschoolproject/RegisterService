572391bd98feff1a1f94a182e175f0d3b17a2a5a
Manjunath SV <stateoftheartschoolproject@gmail.com>
2017-02-06 14:23:41 +0530

    First commit

---
+++ src/main/java/com/school/project/RegisterServiceApplication.java
@@ -0,0 +1,12 @@
+package com.school.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterServiceApplication.class, args);
+	}
+}
