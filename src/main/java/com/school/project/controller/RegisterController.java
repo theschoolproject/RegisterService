080840fc41e0c5b9b6113582acafb2f662b58096
Manjunath SV <stateoftheartschoolproject@gmail.com>
2017-02-13 14:39:31 +0530

    some changes to accept payload

--- src/main/java/com/school/project/controller/RegisterController.java
+++ src/main/java/com/school/project/controller/RegisterController.java
@@ -1,8 +1,19 @@
 package com.school.project.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.school.project.model.School;
 
 @Controller
+@RequestMapping(value = "\register")
 public class RegisterController {
+	@RequestMapping(value = "school",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void register(@RequestBody School school) {
+
+	}
 
 }
---
+++ src/main/java/com/school/project/model/School.java
@@ -0,0 +1,79 @@
+package com.school.project.model;
+
+public class School {
+	
+	private int id;
+	private String name;
+	private String code;
+	private String address;
+	private String email;
+	private int phone;
+	private int fax;
+	private String website;
+	private String bellSchedule;
+	private String grades;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public int getFax() {
+		return fax;
+	}
+	public void setFax(int fax) {
+		this.fax = fax;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getBellSchedule() {
+		return bellSchedule;
+	}
+	public void setBellSchedule(String bellSchedule) {
+		this.bellSchedule = bellSchedule;
+	}
+	public String getGrades() {
+		return grades;
+	}
+	public void setGrades(String grades) {
+		this.grades = grades;
+	}
+	
+	
+	
+
+}
